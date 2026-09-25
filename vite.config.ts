a9ec5813c1eb37f332e38fb47834ddd80dd10290
Chyvantha VIth <chyvanthavith10021995@gmail.com>
2026-09-25 23:21:09 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000
-  }
-})
+    // បង្កើនកម្រិតកំណត់កុំឱ្យចេញ Warning (គិតជា kB)
+    chunkSizeWarningLimit: 1600, 
+    
+    rollupOptions: {
+      output: {
+        // បែងចែកបណ្ណាល័យពី node_modules ឱ្យដាច់ដោយឡែកពីកូដកម្មវិធីយើង
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // អ្នកអាចបំបែកជា vendor ទូទៅ ឬបែងចែកតាមライブラリ (Library) ធំៗ
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
